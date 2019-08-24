2ad81c79c8f6f2819ef08c38a1a983d3d6e24c48
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-24 19:04:16 +0100

    create row html from app.js to be able to display data ''course'' after its added and clear the fields after pressing submit, and insert  X buttom for delete the courses

--- app.js
+++ app.js
@@ -8,11 +8,48 @@ function Course(title, author, isbn) {
 // UI Constructor
 function UI() {}
 
+UI.prototype.addCourseToList = function(course) {
+    const list = document.getElementById('course-list');
+    // create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+        <td>${course.title}</td>
+        <td>${course.author}</td>
+        <td>${course.isbn}</td>
+        <td><a href="#" class="delete">X<a></td>
+    `;
+    list.appendChild(row);
+}
+// Clear fields
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+
+}
+
 
 
 // Event Listeners
-document.getElementById('course-form').addEventListener('submit',function(e){
-    console.log('test');
+document.getElementById('course-form').addEventListener('submit', 
+function(e){
+    // Get form values
+    const title = document.getElementById('title').value,
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
+
+    // Instantiate course
+    const course = new Course(title, author, isbn);
+
+    // Instantiate UI
+    const ui = new UI();
+
+    // Add course to list
+    ui.addCourseToList(course);
+
+    // Clear fields
+    ui.clearFields();
 
     e.preventDefault();
 })
